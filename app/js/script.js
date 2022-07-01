5fb951bdcb7046b27fab53bc52ab7a462a675dcd
Daria <dshaulko@kaaiot.io>
2022-07-01 20:19:41 +0300

    markup ready

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,16 @@
-const testString = 'This is a test!';
+const hamburger = document.querySelector(".navbar__hamburger");
+const navMenu = document.querySelector(".navbar__list");
+const navLink = document.querySelectorAll(".navbar__link");
 
-console.log(testString);
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
-const fruits = [
-  'mango',
-  'papaya',
-  'lychee',
-];
+function mobileMenu() {
+  hamburger.classList.toggle("navbar__hamburger--active");
+  navMenu.classList.toggle("navbar__list--active");
+}
 
-fruits.map((fruit) => {
-  console.log(fruit);
-});
+function closeMenu() {
+  hamburger.classList.remove("navbar__hamburger--active");
+  navMenu.classList.remove("navbar__list--active");
+}
